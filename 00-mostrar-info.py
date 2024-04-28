6f642b865129b1a32fe8d6345c921771c6fa1fc8
Eduardo <114220633+Loquelojonove@users.noreply.github.com>
2024-04-28 14:04:57 +0200

    primer commit

---
+++ 00-mostrar-info.py
@@ -0,0 +1,22 @@
+print("Hola Time of Software!")
+print("Este es mi primer programa en Python")
+
+
+--------Uso de SEP (permite añadir una cadena de texto al final de cada cadena enviada para mostrar por pantalla y sustituir el espacio en blanco).-------------
+print (1,2,3,4,5)
+print(1,2,3,4,5, sep= ",")
+
+------Uso de END (permite añadir una cadena de texto como elemento final del conjunto de cadenas  detexto que se han enviado para mostrar por pantalla).---------
+print(1,2,3,4,5, end= ".")
+print(1,2,3,4,5, sep= ",", end= ".")
+
+----------LEER INFO DESDE TECLADO (INPUT)-------------
+print("Hola. Somos Time of Software. ¿Cómo te llamas?")
+nombre = input()
+print("Nos alegramos mucho de que nos hayas elegido para aprender Pyhton,",nombre,".")
+print("Hola",nombre, "nos alegramos de que hayas elegido este libro para aprender Python.")
+
+
+edad = input("¿Cuántos años tienes?: ")
+print("Tienes", edad, "años. Es una bonita edad.")
+#-------------ALT + 39 = ESCRIBIR COMILLA SIMPLE----------------
